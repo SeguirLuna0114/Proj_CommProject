5acdbe7bd690236dc910e1b6832776d59dae4132
SeguirLuna0114 <seguir.luna@gmail.com>
2023-11-24 09:27:01 +0900

    upload class files for mypage

--- src/main/java/music/controller/MypageController.java
+++ src/main/java/music/controller/MypageController.java
@@ -17,9 +17,28 @@ public class MypageController {
 //		return "home";
 //	}
 	
+	// 마이페이지 메인으로 이동
 	@RequestMapping("mypage.do")
-	public String prList() {
+	public String mypageMain() {
 		return "/mypage/mypage";
+	}
+
+	// 지원완료 페이지로 이동(모든 진행상태)
+	@RequestMapping("applyStatusAll.do")
+	public String mypage_appAll() {
+		return "/mypage/applyStatusAll";
+	}
+	
+	// 지원완료 페이지로 이동(진행상태 = 대기중)
+	@RequestMapping("applyStatus.do")
+	public String mypage_appl() {
+		return "/mypage/applyStatus";
+	}
+	
+	// 지원완료 페이지로 이동(진행상태 = 합격)
+	@RequestMapping("applyStatusFin.do")
+	public String mypage_appfin() {
+		return "/mypage/applyStatusFin";
 	}
 }
 
