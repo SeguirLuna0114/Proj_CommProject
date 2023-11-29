e7d67226ceafc07048fa9c8f454214321c3fee75
SeguirLuna0114 <seguir.luna@gmail.com>
2023-11-30 08:56:12 +0900

    update configuration.xml file for typeAlias

--- src/main/webapp/js/webSocket.js
+++ src/main/webapp/js/webSocket.js
@@ -1,66 +1,149 @@
 /** 웹소켓 관련 사용자 정의 코드 */
 
 /** 웹 소켓 연결 수립 및 해제 */
-var websock;
-$(function() {
-	$('#message').keypress(function(event) {
-		var keycode = event.keyCode ? event.keyCode : event.which;
-		if (keycode == 13)
-			send();
-		event.stopPropagation();
-	});
-	$('#enterBtn').click(function() {
-		connect();
-	});
-	$('#exitBtn').click(function() {
-		disconnect();
-	});
-	$('#sendBtn').click(function() {
-		send();
-	});
-});
-
-function send() {
-	var nickName = $('#nickName').val();
-	var msg = $('#message').val();
-	websocket.send('msg:' + nickName + ' => ' + msg);
-	$('#message').val('');
-}
+let stompClient = null;
+let headerNotificationCount = 0;
+let proposalNotificationCount = 0;
+let applicationNotificationCount = 0;
 
 function connect() {
-	websock = new WebSocket("ws://localhost:80/webSock/chat-ws.do");
-	websock.onopen = onOpen;
-//	websock.onclose = onClose;
-	websock.onmessage = onMessage;
+    let socket = new SockJS('/our-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        updateHeaderNotificationDisplay();
+        updateProposalNotificationDisplay();
+        updateApplicationNotificationDisplay();
+        stompClient.subscribe('/topic/all', function (message) {
+            showMessage(JSON.parse(message.body).content);
+        });
+
+        stompClient.subscribe('/topic/proposal', function (message) {
+            showMessage(JSON.parse(message.body).content);
+        });
+
+        stompClient.subscribe('/topic/application', function (message) {
+            showMessage(JSON.parse(message.body).content);
+        });
+
+        stompClient.subscribe('/user/topic/private-messages', function (message) {
+            showMessage(JSON.parse(message.body).content);
+        });
+
+        stompClient.subscribe('/topic/global-notifications', function () {
+            headerNotificationCount = headerNotificationCount + 1;
+            updateHeaderNotificationDisplay();
+        });
+
+        stompClient.subscribe('/topic/proposal-notifications', function () {
+            proposalNotificationCount = proposalNotificationCount + 1;
+            updateProposalNotificationDisplay();
+        });
+
+        stompClient.subscribe('/topic/application-notifications', function () {
+            applicationNotificationCount = applicationNotificationCount + 1;
+            updateApplicationNotificationDisplay();
+        });
+
+        stompClient.subscribe('/user/topic/private-notifications', function (message) {
+            headerNotificationCount = headerNotificationCount + 1;
+            updateHeaderNotificationDisplay();
+        });
+    });
 }
 
-function disconnect() {
-	websock.close();
+function showMessage(message) {
+    $(".messages").append("<p>" + message + "</p>");
 }
 
-function send() {
-	var nickname = $('#nickName').val();
-	var message = $('#message').val();
-	websock.send('msg:' + nickname + ' : ' + message);
-	$('#message').val('');
+function sendFirstAlarm() {
+    console.log("sending first alarm");
+    stompClient.send("/ws/firstalarm");
 }
 
-function onOpen(event) {
-	appendMessage("연결되었습니다.");
+function sendProposalAlarm() {
+    console.log("sending proposal alarm");
+    stompClient.send("/ws/proposalalarm");
 }
 
-function onClose(event) {
-	appendMessage("연결이 종료되었니다.");
+function sendApplicationAlarm() {
+    console.log("sending application alarm");
+    stompClient.send("/ws/applicationalarm");
 }
 
-function onMessage(event) {
-	var data = event.data;
-	appendMessage(data);
+function sendPrivateMessage() {
+    console.log("sending private message");
+    stompClient.send("/ws/private-message", {}, JSON.stringify({ 'messageContent': $("#private-message").val() }));
 }
 
-function appendMessage(msg) {
-	$('#chatMessageArea').append(msg + "<br>");
-	var chatAreaheight = $('#chatArea').height();
-	var maxscroll = $('#chatMessageArea').height() - chatAreaheight;
-	$('#chatArea').scrollTop(maxscroll);
+function updateHeaderNotificationDisplay() {
+    if (headerNotificationCount == 0) {
+        $(".notifications").hide();
+    } else {
+        $(".notifications").show();
+        $(".notifications").text(headerNotificationCount);
+    }
 }
+
+function updateProposalNotificationDisplay() {
+    if (proposalNotificationCount == 0) {
+        $(".proposal_notifications").hide();
+    } else {
+        $(".proposal_notifications").show();
+        $(".proposal_notifications").text(proposalNotificationCount);
+    }
+}
+
+function updateApplicationNotificationDisplay() {
+    if (applicationNotificationCount == 0) {
+        $(".application_notifications").hide();
+    } else {
+        $(".application_notifications").show();
+        $(".application_notifications").text(applicationNotificationCount);
+    }
+}
+
+function resetHeaderNotificationCount() {
+    headerNotificationCount = 0;
+    updateHeaderNotificationDisplay();
+}
+
+function resetProposalNotificationCount() {
+    proposalNotificationCount = 0;
+    updateProposalNotificationDisplay();
+}
+
+function resetApplicationNotificationCount() {
+    applicationNotificationCount = 0;
+    updateApplicationNotificationDisplay();
+}
+
+$("#btnLogin").click(function () {
+    login();
+});
+
+$(".btn_proposal").click(function () {
+    sendProposalAlarm();
+});
+
+$(".btn_application").click(function () {
+    sendApplicationAlarm();
+});
+
+$("#send-private").click(function () {
+    sendPrivateMessage();
+});
+
+$(".notifications").click(function () {
+    resetHeaderNotificationCount();
+});
+
+$(".proposal_notifications").click(function () {
+    resetProposalNotificationCount();
+});
+
+$(".application_notifications").click(function () {
+    resetApplicationNotificationCount();
+});
+
+connect();
