8c0c5d1c0c365632696b15f6c75972345e92ab49
SeguirLuna0114 <seguir.luna@gmail.com>
2023-12-13 08:17:13 +0900

    update obriproject - with websocket alert

--- src/main/java/music/websocket/EchoHandler.java
+++ src/main/java/music/websocket/EchoHandler.java
@@ -42,8 +42,9 @@ public class EchoHandler extends TextWebSocketHandler{
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		sessions.add(session);
 		
-		String senderId = session.getId();
 		String senderEmail = getEmail(session);
+		String senderId = session.getId();
+		System.out.println(">>>>>>>>>"+senderId);
 		
 		userSessionMap.put(session.getId(), session);
 	}
@@ -72,16 +73,57 @@ public class EchoHandler extends TextWebSocketHandler{
 		// message.getPayload()함수 : 메시지에 담긴 텍스트값을 얻을 수 있음
 		System.out.println(session.getId()+"가 보낸 메시지: "+message.getPayload());
 		
+		// 메시지에서 정보 추출
 		String msg = message.getPayload();
 		if(!StringUtils.isNullOrEmpty(msg)) {
-			String[] strs = msg.spl
+			String[] strs = msg.split(",");
+			
+			// 메시지가 유효하고 길이가 5(strs.length == 5)인 경우
+			if(strs != null) {
+				String cmd = strs[0];  // 명령어 유형
+	            String caller = strs[1];  // 댓글 작성자
+	            String receiver = strs[2];  // 게시글 작성자
+	            String receiverEmail = strs[3];  // 게시글 작성자의 이메일
+	            String seq = strs[4];  // 게시글 번호
+	            
+	            // 작성자가 로그인 중인 경우
+	            WebSocketSession boardWriterSession = userSessionMap.get(receiver);
+	            
+	            // cmd = reply : 댓글인 경우
+	            if("reply".equals(cmd) && boardWriterSession != null) {
+	            	TextMessage tmpMsg = new TextMessage(caller + "님이 " + 
+                            "<a type='external' href='/mentor/menteeboard/menteeboardView?seq="+seq+"&pg=1'>" 
+	            			+ seq + "</a> 번 게시글에 댓글을 남겼습니다.");
+	            	boardWriterSession.sendMessage(tmpMsg);
+	            
+	            // cmd = message : 쪽지를 보낸 경우
+	            } else if("message".equals(cmd) && boardWriterSession != null) {
+	                TextMessage tmpMsg = new TextMessage(caller + "님이 " +
+                            "<a type='external' href='/mentor/essayboard/essayboardView?pg=1&seq="+seq+"&mentors="+ receiver +"'>" 
+	                		+ receiver  + "</a>님에게 쪽지를 보냈습니다.");
+	                boardWriterSession.sendMessage(tmpMsg);	            	
+	            }
+			}
+			
+			// 공고 지원자인경우
+	        if(strs != null && strs.length == 5) {
+	            String cmd = strs[0];
+	            String mentee_name = strs[1];
+	            String mentor_email = strs[2];
+	            String meetingboard_seq = strs[3];
+	            String participation_seq = strs[4];
+	            
+	            // 모임 작성자가 로그인 중인 경우
+	            WebSocketSession mentorSession = userSessionMap.get(mentor_email);
+	            if(cmd.equals("apply") && mentorSession != null) {
+	                TextMessage tmpMsg = new TextMessage(
+	                        mentee_name + "님이 공고에 지원했습니다. " 
+	                        +"<a type='external' href='/mentor/participation/participationView?mseq="+ meetingboard_seq +"&pseq="+ participation_seq +"'>신청서 보기</a>");
+	                mentorSession.sendMessage(tmpMsg);
+	            }
+	        }			
 		}
 		
-		// dao.count_receive_note 함수 : 수신자의 닉네임을 통해서 수신함에서 현재 수신자에게 몇개의 쪽지가 와있는지를 카운트
 		
-		// 인터페이스에 정의된 메소드들을 구현한 객체를 가져옴
-		mypageDao dao = sqlsession.getMapper(mypageDao.class);
-		
-		session.sendMessage(new TextMessage(dao.cntMgsAlert(message.getPayload()))); 
 	}
 }
